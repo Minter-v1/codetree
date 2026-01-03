235fdea8a2893af86eb3c177746a2589c0ec8a4c
Minter <adf5858@naver.com>
2026-01-03 22:17:21 +0900

    Title: 사칙연산 연습, Time: 85ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 260103/사칙연산 연습/practice-basic-arithmetic-operations.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 5, b = 30;
+        int c = a + b;
+        int div = c / 8, mod = c % 8;
+
+        System.out.println(div);
+        System.out.println(mod);
+
+    }
+}
