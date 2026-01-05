66f41de9ca68b577b44d016d29f9c32398639b80
Minter <adf5858@naver.com>
2026-01-05 23:48:33 +0900

    Title: 시험 통과 여부 확인하기, Time: 129ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260105/시험 통과 여부 확인하기/verify-test-passed.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int score = sc.nextInt();
+
+        if (score < 80) {
+            System.out.println(80-score + " more score");
+        } else {
+            System.out.println("pass");
+        }
+
+    }
+}
