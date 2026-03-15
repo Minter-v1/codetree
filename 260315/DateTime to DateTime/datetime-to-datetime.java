f1c81e910c47e5b737b27e3996607fd7ab1fb7da
Minter <adf5858@naver.com>
2026-03-15 18:42:58 +0900

    Title: DateTime to DateTime, Time: 154ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 260315/DateTime to DateTime/datetime-to-datetime.java
+++ 260315/DateTime to DateTime/datetime-to-datetime.java
@@ -6,9 +6,38 @@ public class Main {
         int B = sc.nextInt();
         int C = sc.nextInt();
         // Please write your code here.
+
+        System.out.println(checkDateTime(A, B, C));
     }
 
     static int checkDateTime(int a, int b, int c) {
-        
+       if (a < 11) return -1;
+       if ((a > 11) && (b < 11)) return -1;
+       if ((a > 11) && (b < 11) && (c < 11)) return -1;
+
+       int elapsedMin = 0;
+       int d = 11, h = 11, m = 11;
+
+       while (true) {
+        if ((d == a) && (h == b) && (m == c)) {
+            break;
+        }
+
+        m++;
+        elapsedMin++;
+        if (m == 60) {
+            m = 0;
+            h++;
+        }
+
+        if (h == 24) {
+            h = 0;
+            d++;
+        }
+
+       }
+
+       return elapsedMin;
     }
+}
        
