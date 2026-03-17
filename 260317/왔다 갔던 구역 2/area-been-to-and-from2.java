099031d869ef9a615e8611493f475e2f3236b4ac
Minter <adf5858@naver.com>
2026-03-17 17:22:22 +0900

    Title: 왔다 갔던 구역 2, Time: 129ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260317/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[201];
+        int temp = 100;
+
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int dist = sc.nextInt();
+            String dir = sc.next();
+
+            int start = temp;
+            int end;
+
+            
+
+            // 오른쪽으로 가는 경우
+            if (dir.equals("R")) {
+                end = temp + dist - 1;
+                temp = end + 1;
+
+                for (int k = start; k <= end; k++) {
+                    arr[k]++;
+                }
+            } else {
+            // 왼쪽으로 가는 경우
+                end = temp - dist + 1;
+                temp = end - 1;
+
+                for (int k = start; k >= end; k--) {
+                    arr[k]++;
+                }
+            }
+
+        }
+
+
+        System.out.println(findExtend(arr));
+
+
+    }
+
+    static int findExtend(int[] arr) {
+        int cnt = 0;
+
+        for (int i = 0; i < 200; i++) {
+            if (arr[i] >= 2) {
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
+}
