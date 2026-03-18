2bda0904cd6ada1b6d5902527c19ed81db7c4285
Minter <adf5858@naver.com>
2026-03-18 22:41:55 +0900

    Title: 십진수와 이진수 2, Time: 150ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 260318/십진수와 이진수 2/decimal-and-binary-number-2.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String n = sc.nextLine();
+        char[] arr = n.toCharArray();
+
+        changeDigit(changeDecimal(arr));
+
+
+
+    }
+
+    // 이진수 변환
+    static void changeDigit(int num) {
+        int[] arr = new int[10];
+        int tmp = 0;
+
+        while (true) {
+            if (num == 0) break;
+
+            arr[tmp++] = num % 2;
+            num /= 2;
+        }
+
+        for (int i = tmp - 1; i >= 0; i--) {
+            System.out.print(arr[i]);
+        }
+        
+        return;
+    }
+
+    static int changeDecimal(char[] arr) {
+        int mul = arr.length - 1;
+        int res = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            res += ((int)arr[i] - '0') * Math.pow(2,mul);
+            mul--;
+        }
+
+        return res * 17;
+    }
+}
