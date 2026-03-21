5c3cf3db15d41c6942684b291694e6fbd449d959
Minter <adf5858@naver.com>
2026-03-21 16:12:42 +0900

    Title: 색종이의 총 넓이, Time: 126ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 260321/색종이의 총 넓이/the-total-area-of-colored-paper.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[][] arr = new int[101][101];
+        int area = 0;
+
+        for (int i = 0; i < n; i++) {
+            int x1 = sc.nextInt();
+            int y1 = sc.nextInt();
+
+            x1 += 100;
+            y1 += 100;
+
+            for (int r = x1; r < x1 + 8; r++) {
+                for (int c = y1; c < y1 + 8; c++) {
+                    arr[r][c]++;
+                }
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] == 0) continue;
+                area++;
+            }
+        }
+
+        System.out.println(area);
+
+
+        
+    }
+}
