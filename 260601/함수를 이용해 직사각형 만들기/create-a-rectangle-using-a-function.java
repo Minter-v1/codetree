e5573e64c95b662540e6d386a963447d6b6a26c6
Minter <adf5858@naver.com>
2026-06-01 21:40:16 +0900

    [Passed] 함수를 이용해 직사각형 만들기 (142ms, 10MB)

---
+++ 260601/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(
+            new InputStreamReader(System.in)
+        );
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        for (int r = 0; r < n; r++) {
+            for (int c = 0; c < m; c++) {
+                System.out.print(1);
+            }
+
+            System.out.println();
+        }
+    }
+}
