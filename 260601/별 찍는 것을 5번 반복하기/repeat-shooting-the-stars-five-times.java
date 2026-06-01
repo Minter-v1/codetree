cd6dda2577312be2aba10ab8609afb7b0ee87b42
Minter <adf5858@naver.com>
2026-06-01 21:21:29 +0900

    [Passed] 별 찍는 것을 5번 반복하기 (89ms, 9MB)

---
+++ 260601/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int r = 0; r < 5; r++) {
+            for (int c = 0; c < 10; c++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
