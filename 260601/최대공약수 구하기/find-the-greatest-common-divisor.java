1e2eb39d159d5927439e153d5f2a465332c4c772
Minter <adf5858@naver.com>
2026-06-01 21:58:46 +0900

    [Passed] 최대공약수 구하기 (96ms, 9MB)

---
+++ 260601/최대공약수 구하기/find-the-greatest-common-divisor.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        System.out.println(gcd(n, m));
+
+
+    }
+
+    static int gcd(int a, int b) {
+        if (b != 0) {
+            return gcd(b, a % b);
+        }
+        return a;
+    }
+}
