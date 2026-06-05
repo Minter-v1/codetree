681bbcb38ec542147e36aa7e8e9bcabf50c76fe7
Minter <adf5858@naver.com>
2026-06-05 15:33:16 +0900

    [Passed] 재귀함수를 이용한 별 출력 (114ms, 9MB)

---
+++ 260605/재귀함수를 이용한 별 출력/star-output-with-recursive-function.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        fn(n);
+
+    }
+
+    static void fn(int n) {
+        if (n == 1) {
+            System.out.println("*");
+            return;
+        }
+        
+        fn(n-1);
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("*");
+        }
+
+        System.out.println();
+
+
+    }
+}
