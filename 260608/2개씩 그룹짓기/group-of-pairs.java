24162db6da89d5fddac89ffbb400474735844507
Minter <adf5858@naver.com>
2026-06-08 13:39:42 +0900

    [Passed] 2개씩 그룹짓기 (114ms, 10MB)

---
+++ 260608/2개씩 그룹짓기/group-of-pairs.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[2*n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < 2*n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            int temp = arr[i] + arr[2*n-1-i];
+
+            if (temp > max) {
+                max = temp;
+            }
+        }
+
+        System.out.println(max);
+
+
+    }
+
+
+}
