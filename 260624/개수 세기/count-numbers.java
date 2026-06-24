2f5ecda39fbb6e5f691dc9011bd010956e3260fe
Minter <adf5858@naver.com>
2026-06-24 17:55:32 +0900

    [Passed] 개수 세기 (90ms, 9MB)

---
+++ 260624/개수 세기/count-numbers.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int cnt = 0;
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            if (Integer.parseInt(st.nextToken()) == m) {
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+
+
+    }
+}
